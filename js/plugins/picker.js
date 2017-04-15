74b29ef41a12deef14b6aab523f3de14fee62d36
viewweiwu <648764190@qq.com>
2017-04-16 02:24:13 +0800

    fix picker

--- js/plugins/picker.js
+++ js/plugins/picker.js
@@ -3,29 +3,34 @@
 		this.$container = $(".container"); // 最外层容器啦
 		this.$target = $target;
 		this.id = opts.id ? opts.id : $target.attr("id");
+		this.selectText = opts.text;
+		this.selectValue = opts.value;
 		this.data = opts.data;
+		this.list = [];
+		this.split = " - ";
+		this.defaultText = "请选择";
 		this.init();
 	}
 
 	Picker.prototype = {
 		init: function() {
 			this.createEl();
+			this.list.push(new List(this.data, this));
 			this.initStyle();
 			this.bind();
 		},
 		createEl: function() {
 			var html = '';
 			var $el = '';
-			var list = this.getListEl(this.data);
 			html += 
         '<div class="picker" id="'+ this.id + '">'+
           '<div class="pnl">'+
+          	'<div class="decorate"></div>' +
             '<div class="top">'+
               '<button class="btn not-bg btn-cancel">取消</button>'+
               '<button class="btn not-bg btn-confirm">完成</button>'+
             '</div>'+
             '<div class="content">'+
-            	list +
             '</div>'+
           '</div>'+
         '</div>';
@@ -34,105 +39,21 @@
 
 			this.$el = $el;
 			this.$pnl = $el.find(".pnl");
+			this.$content = $el.find(".content");
 			this.$cancelBtn = $el.find(".btn-cancel");
 			this.$confirmBtn = $el.find(".btn-confirm");
-			this.$list = $el.find(".list");
-			this.singleHeight = this.$list.find(".item").height(); // 1 个单位 = 单个 li 的 height
+			this.$decorate = $el.find(".decorate");
 		},
-		getListEl: function(data) {
-			var result = '';
-			if($.isArray(data)) {
-				result += `
-          <div class="scroll-pnl">
-            <ul class="list">
-        `;
-				$.each(data || [], function(i, obj) {
-					result += '<li class="item">' + obj + '</li>';
-				});
-				result += ` 
-            </ul>
-          </div>
-        `;
-			}
-			return result;
-		},
-		/**
-		 * 名称: 初始化样式
-		 * 作用: 将所有 list 向下移动 3 个单位
-		 */
 		initStyle: function() {
-			var defalutY = this.singleHeight * 3;
-			this.$list.css("transform", "translateY(" + defalutY + "px)");
-			this.defalutY = defalutY;
+			this.$decorate.height(this.list[0].getSingleHeight());	
 		},
 		bind: function() {
-			this.$target.on("focus",  this.onTargetFocus.bind(this));
+			this.$target.on({
+				"focus": this.onTargetFocus.bind(this),
+				"change": this.onTargetChange.bind(this)
+			});
 			this.$cancelBtn.on("click", this.onCancelBtnClick.bind(this));
 			this.$confirmBtn.on("click", this.onConfirmBtnClick.bind(this));
-			this.$list.on({
-				"touchstart": this.onStart.bind(this),
-				"touchmove": this.onMove.bind(this),
-				"touchend": this.onEnd.bind(this)
-			});
-		},
-		/**
-		 * 名称: 鼠标 touchstart 事件
-		 * 作用: 标记当前 list 的 pageY
-		 */
-		onStart: function(e) {
-			var pageY = e.touches[0].pageY;
-			this.pageY = pageY;
-		},
-		/**
-		 * 名称: 鼠标 touchmove 事件
-		 * 作用: 计算跟之前标记的 pageY 的差值，并累加到 translateY 上
-		 */
-		onMove: function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			var $target = this.getTarget(e);
-			var pageY = e.touches[0].pageY;
-			var y = this.getY($target); // 获取之前的 translateY
-			var final = pageY - this.pageY + y; // 比较差值并累加，计算出最终值
-			$target.css({
-				"transform": "translateY(" + final + "px)",
-				"transition": ""
-			});
-			this.pageY = pageY; // 重新标记 pageY
-		},
-		/**
-		 * 名称: 鼠标 touchend 事件
-		 * 作用: 矫正最终的滚动值
-		 */
-		onEnd: function(e) {
-			var $target = this.getTarget(e);
-			var $li = $target.find("li");
-			var totalHeight = $li.length * this.singleHeight; // 获取所有 li 的总高度
-			var y = this.getY($target) - this.defalutY; // 获取本身偏移了多少 Y
-			var bottom = (totalHeight - this.singleHeight) * -1; // 底部边界
-			var final = y + this.defalutY; // 默认值最终值
-			var diff = 0;
-			// 判断最终值是否超出边界，矫正到顶部边界或者底部边界
-			if(y > 0) {
-				final = this.defalutY;
-			} else if(y < bottom) {
-				final = bottom + this.defalutY;
-			}
-
-			// 计算需要矫正的差
-			diff = final % this.singleHeight;
-
-			// 矫正未移动到正确位置的最终值，如果差值大于一半，则最终值加上一个单位，反之减去一个单位
-			if(diff < this.singleHeight / 2) {
-				final = final - diff;
-			} else {
-				final = final - diff + this.singleHeight;
-			}
-
-			$target.css({
-				"transform": "translateY(" + final + "px)",
-				"transition": "transform 300ms"
-			});
 		},
 		onTargetFocus: function(e) {
 			this.$target.blur();
@@ -142,8 +63,44 @@
 			this.close();
 		},
 		onConfirmBtnClick: function(e) {
+			var self = this;
+			var text = "";
+			var value = "";
 			this.close();
-			this.$target.val(this.getText());
+			$.each(this.list, function(i, obj) {
+				if(obj.getIndex() === 0) return true;
+				if(i !== 0 ) {
+					text += self.split;
+					value += self.split;
+				}
+				text += obj.getText();
+				value += obj.getValue();
+			});
+			this.$target.val(text);
+			this.$target.attr("data-value", value);
+		},
+		onTargetChange: function(e, index) {
+			if(this.list[index]) {
+				var target = this.list[index];
+				var data = target.data[target.getIndex() - 1] && target.data[target.getIndex() - 1]["children"];
+				
+				if(target.getIndex() === 0) {
+					$.each(this.list.slice(index + 1), function(i, obj) {
+							obj.destroy();
+					});
+					this.list.length = index + 1;
+					return;
+				}
+				
+				if(data) {
+					$.each(this.list.slice(index + 1), function(i, obj) {
+							obj.destroy();
+					});
+					this.list.length = index + 1;
+					this.list.push(new List(data, this));
+				}
+			}
+			
 		},
 		show: function() {
 			var self = this;
@@ -168,21 +125,193 @@
 					"transform": "translateX(100%)"
 				})
 			}, 300);
+		}
+	};
+
+	function List(data, picker) {
+		this.$container = picker.$container; // 最外层容器啦
+		this.$picker = picker.$el;
+		this.$pnl = picker.$pnl;
+		this.$content = picker.$content;
+		this.$target = picker.$target;
+		this.selectText = picker.selectText;
+		this.selectValue = picker.selectValue;
+		this.defaultText = picker.defaultText;
+		this.data = data;
+		this.init();
+	}
+	
+	List.prototype = {
+		init: function() {
+			this.createEl();
+			this.initStyle();
+			this.bind();
 		},
-		getText: function(e) {
-			var self = this;
+		createEl: function() {
+			var $html = $(this.getHtml(this.data));
+			this.$content.append($html);
+			this.$el = $html.find(".list");
+			this.singleHeight = this.$el.find(".item").height(); // 1 个单位 = 单个 li 的 height
+			this.$el.find(".item").height(this.singleHeight);
+		},
+		getSingleHeight: function() {
+			return this.singleHeight;
+		},
+		getHtml: function(data) {
 			var result = "";
-
-			$.each(this.$list, function(i, obj) {
-				var $list = $(obj);
-				var y = (self.getY($list) - self.defalutY) * -1;
-				var index = y / self.singleHeight;
-				var $target = $list.find(".item").eq(index);
-				result += $target.text();
-			});
-
+			var self = this;
+			
+			result += 
+				'<div class="scroll-pnl">'+
+					'<ul class="list">' +
+						'<li class="item">' + this.defaultText + '</li>';
+			
+						$.each(data || [], function(i, obj) {
+							var text = "";
+							var value = "";
+							if(typeof obj === "string" || typeof obj === "number") {
+								result += '<li class="item" data-index="' + i + '">' + obj + '</li>';
+							} else {
+								result += '<li class="item" data-index="' + i + '" data-value=' + obj[self.selectValue] + '>' + obj[self.selectText] + '</li>';
+							}
+						});
+			
+			result +=
+					'</ul>'+
+				'</div>';
 			return result;
 		},
+		/**
+		 * 名称: 初始化样式
+		 * 作用: 将所有 list 向下移动 3 个单位
+		 */
+		initStyle: function() {
+			var defalutY = this.singleHeight * 3;
+			this.$el.css("transform", "translateY(" + defalutY + "px)");
+			this.defalutY = defalutY;
+		},
+		bind: function() {
+			this.$el.on({
+				"touchstart": this.onStart.bind(this),
+				"touchmove": this.onMove.bind(this),
+				"touchend": this.onEnd.bind(this)
+			});
+		},
+		
+		/**
+		 * 名称: 鼠标 touchstart 事件
+		 * 作用: 标记当前 list 的 pageY
+		 */
+		onStart: function(e) {
+			var pageY = e.touches[0].pageY;
+			this.pageY = pageY;
+			this.startDate = new Date();
+			this.startY = util.getY(util.getTarget(e)) - this.defalutY;
+			this.currIndex = this.getIndex();
+		},
+		/**
+		 * 名称: 鼠标 touchmove 事件
+		 * 作用: 计算跟之前标记的 pageY 的差值，并累加到 translateY 上
+		 */
+		onMove: function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			var $target = util.getTarget(e);
+			var pageY = e.touches[0].pageY;
+			var y = util.getY($target); // 获取之前的 translateY
+			var final = pageY - this.pageY + y; // 比较差值并累加，计算出最终值
+			$target.css({
+				"transform": "translateY(" + final + "px)",
+				"transition": ""
+			});
+			this.pageY = pageY; // 重新标记 pageY
+		},
+		/**
+		 * 名称: 鼠标 touchend 事件
+		 * 作用: 矫正最终的滚动值
+		 */
+		onEnd: function(e) {
+			var self = this;
+			var $target = util.getTarget(e);
+			var $li = $target.find("li");
+			var totalHeight = $li.length * this.singleHeight; // 获取所有 li 的总高度
+			var y = util.getY($target) - this.defalutY; // 获取本身偏移了多少 Y
+			var bottom = (totalHeight - this.singleHeight) * -1; // 底部边界
+			var final = y + this.defalutY; // 默认值最终值
+			var diff = 0; // 矫正差值
+			var dateDiff = new Date() - this.startDate; // 于开始的滚动时间做判断 
+			var diffY = this.startY - y; // 计算滚动惯性
+			
+			// 矫正边界 判断最终值是否超出边界，矫正到顶部边界或者底部边界
+			if(y > 0) {
+				final = this.defalutY;
+			} else if(y < bottom) {
+				final = bottom + this.defalutY;
+			}
+
+			// 计算需要矫正的差
+			diff = final % this.singleHeight;
+
+			// 矫正未移动到正确位置的最终值，如果差值大于一半，则最终值加上一个单位，反之减去一个单位
+			if(diff < this.singleHeight / 2) {
+				final = final - diff;
+			} else {
+				final = final - diff + this.singleHeight;
+			}
+				
+			// 惯性滚动
+			if(true) {
+				var direction = diffY > 0 ? 1 : -1;
+				var d = Math.round(Math.abs(diffY) / this.singleHeight * 2); // 移动单位
+				var t = 3 - Math.ceil(dateDiff / 100); // 移动时间
+				if(t < 1) t = 0;
+				var a = t * d * direction * this.singleHeight;
+				
+				final -= a;
+				
+				// 再次矫正
+				if(final < bottom + 3 * this.singleHeight) {
+					final = bottom + 3 * this.singleHeight;
+				} else if(final > this.defalutY) {
+					final = this.defalutY;
+				}
+			}
+			
+			$target.css({
+				"transform": "translateY(" + final + "px)",
+				"transition": "transform 300ms"
+			});
+			
+			self.timer = setTimeout(function() {
+				var index = self.getIndex();
+				if(self.timer) {
+					clearTimeout(self.timer);
+				}
+				if(self.currIndex !== index) {
+					self.$target.trigger("change", self.$el.parent().index());
+				}
+			}, 300);
+		},
+		getIndex: function() {
+			var self = this;
+			var result = "";
+			var y = (util.getY(this.$el) - self.defalutY) * -1;
+			var index = Math.round(y / self.singleHeight);
+
+			return index;
+		},
+		getText: function() {
+			return this.$el.find(".item").eq(this.getIndex()).text();
+		},
+		getValue: function() {
+			return this.$el.find(".item").eq(this.getIndex()).data("value");
+		},
+		destroy: function() {
+			this.$el.parent().remove();
+		}
+	}
+	
+	var util = {
 		/**
 		 * 名称: 获取目标
 		 * 作用: 获取 $list 正确的 $target
@@ -202,7 +331,7 @@
 			return parseInt($target.css("transform").slice(11)) || 0;
 		}
 	}
-
+	
 	$.fn.picker = function(options) {
 		return this.each(function(i, obj) {
 			var $target = $(obj);
