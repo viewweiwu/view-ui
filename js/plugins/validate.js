394c66da934aae0be848752bf7abe804c6955ea4
djin.fang <823224424@qq.com>
2017-04-20 17:51:29 +0800

    commit

--- js/plugins/validate.js
+++ js/plugins/validate.js
@@ -11,25 +11,40 @@
     Validate.prototype = {
         init: function() {
             this.getCheckResult();
+            this.onfocus();
+
         },
         getCheckResult: function() {
             var result = this.setCheck();
             return result;
         },
+        onfocus: function($target) {
+            var self = this;
+            $.each(self.inputArr, function(i, iObj) {
+                var $target = $(iObj);
+                if ($target.parents('.bg-warning')) {
+                    $target.bind('focus', function() {
+                        $target.parents('.bg-warning').removeClass('bg-warning');
+                    });
+                }
+            })
+        },
         setCheck: function() {
             //返回验证结果
             var self = this;
             var checkResult = true;
-            $.each(this.inputArr, function(i, iObj) {
+            $.each(self.inputArr, function(i, iObj) {
                 var $target = $(iObj);
                 self.ruleArr = $target.attr('data-validate').split(','); //找到这些input框的验证值 
                 self.inputValue = $target.val(); //找到这些input框的值
                 self.labelText = $target.data("label") || ''; //找到这些input框的值
                 // $target.siblings("label").text().repalce('：', '');
-                $.each(self.ruleArr, function(j, jObj) {
-                    checkResult = self.checkBind(jObj, self.inputValue, self.labelText);
-                    return !checkResult ? false : true;
-                })
+                if (self.ruleArr[0] != '') {
+                    $.each(self.ruleArr, function(j, jObj) {
+                        checkResult = self.checkBind(jObj, self.inputValue, self.labelText, $target);
+                        return !checkResult ? false : true;
+                    })
+                }
                 return !checkResult ? false : true;
             })
             return checkResult
@@ -38,7 +53,7 @@
             var tip = this.setTips(status, label);
             $.Toast({
                 position: "center",
-                content: tip
+                content: tip,
             });
             return false;
         },
@@ -52,40 +67,49 @@
                 integer: '请输入整数',
                 url: '请输入链接',
                 password: '请输入6到30位密码',
-                confpwd: '两次输入的密码不一致'
+                confpwd: '两次输入的密码不一致',
+                postCode: '请输入正确的邮编'
             }
             return validateTips[status];
         },
-        checkBind: function(rule, value, label) {
+        checkBind: function(rule, value, label, input) {
             var self = this;
             value = $.trim(value); //去掉首尾空格
-            var testRule = true;
+            var testRule = value != ''; //是否通过,默认判断是否为空
             /**
              * 文本格式验证正则表达式
              */
             var regex = {
-                empty: /^.+\S$/, //是否为空
                 email: /^(\w)+(\W\w+)*@(\w)+(\-\w+)*((\.\w+)+)$/, // email
                 phone: /^\(?\d{2,3}[- ]?\d{1,3}\)?[- ]?\d{3,4}[- ]?\d{4}$/, // 手机号码
                 tell: /\d{3}-\d{8}|\d{4}-\d{7,8}/, // 固话
                 number: /^[\-\+]?((\d+)([\.,](\d+))?|([\.,](\d+))+)$/, // 数字
                 integer: /^[\-\+]?((\d+))$/, // 整数
                 url: /^[a-zA-z]+:\/\/(\w+(-\w+)*)(\.(\w+(-\w+)*))*/, // 链接
-                password: /^[\w~!@#$%^&*()_+{}:"<>?\-=[\];\',.\/]{6,30}$/ // 密码
+                password: /^[\w~!@#$%^&*()_+{}:"<>?\-=[\];\',.\/]{6,30}$/, // 密码
+                postCode: /^\d{6}$/ //邮编
             };
-
-            if (rule.search(/confpwd/) > -1) {
-                var ele = /confpwd\((.*)\)/.exec(rule)[1];
-                testRule = value == $(ele).val();
-                rule = 'confpwd';
+            if (testRule) {
+                if (rule.search(/confpwd/) > -1) {
+                    var ele = /confpwd\((.*)\)/.exec(rule)[1];
+                    testRule = value == $(ele).val();
+                    rule = 'confpwd';
+                } else if (rule.search(/empty/) > -1) {
+                    rule = 'empty';
+                } else {
+                    testRule = regex[rule].test(value);
+                }
             } else {
-                testRule = regex[rule].test(value);
+                rule = 'empty';
             }
+
 
             if (testRule == false) {
                 self.showTip(rule, label);
+                input.parents('.field').addClass('bg-warning');
                 return false;
             } else {
+                input.parents('.field').removeClass('bg-warning');
                 return true
             }
         },
