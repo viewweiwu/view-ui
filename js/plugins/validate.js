d2bec7b88259141378fe467d0141c402b53b2a24
djin.fang <823224424@qq.com>
2017-04-25 15:02:44 +0800

    更新数字长度验证

--- js/plugins/validate.js
+++ js/plugins/validate.js
@@ -15,10 +15,10 @@
             var result = this.setCheck();
             return result;
         },
-        onfocus: function($input) {
-            $input.bind('focus', function() {
+        onFocus: function($input) {
+            this.$form.on('focus', 'input', function() {
                 $(this).parents('.bg-warning').removeClass('bg-warning');
-            });
+            })
         },
         setCheck: function() {
             //返回验证结果
@@ -59,83 +59,131 @@
                 checkbox: '请同意' + label,
                 orCheck: '请正确输入' + label,
                 idCard: '请正确输入您的身份证号码',
-                custom: label,
+                custom: label || '格式错误',
+                select: label || '格式错误',
+                andCheck: label || '格式错误',
+                max: '请输入少于' + label + '的字符',
+                min: '请输入不少于' + label + '的字符',
             }
             return validateTips[status];
         },
-        check: function(rule, $input) {
-            var self = this,
-                inputValue = $input.val(), //找到这些input框的值
-                labelType = $input.attr('type'), //找到这些input框的类型
-                result = true; //验证结果
-            //先做类型判断
-            if (labelType == "checkbox") {
+        checkBox: function(rule, $input) {
+            if (rule == "checkbox") {
                 return $input.is(':checked');
             } else {
-                inputValue = $.trim(inputValue); //去掉首尾空格
-                var result = true;
-                //文本格式验证正则表达式
-                var regex = {
-                    email: /^(\w)+(\W\w+)*@(\w)+(\-\w+)*((\.\w+)+)$/, // email
-                    phone: /^1[3|4|5|7|8]\d{9}$/, // 手机号码
-                    tell: /\d{3}-\d{8}|\d{4}-\d{7,8}/, // 固话
-                    number: /^[\-\+]?((\d+)([\.,](\d+))?|([\.,](\d+))+)$/, // 数字
-                    integer: /^[\-\+]?((\d+))$/, // 整数
-                    url: /^[a-zA-z]+:\/\/(\w+(-\w+)*)(\.(\w+(-\w+)*))*/, // 链接
-                    password: /^[\w~!@#$%^&*()_+{}:"<>?\-=[\];\',.\/]{6,30}$/, // 密码
-                    postCode: /^\d{6}$/, //邮编
-                    idCard1: /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/, //15位身份证
-                    idCard2: /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/, //18位身份证
-                };
-
-                if (rule.search(/confpwd/) > -1) {
-                    var ele = /confpwd\((.*)\)/.exec(rule)[1];
-                    return inputValue == $(ele).val();
-                } else if (rule == 'empty') {
-                    return inputValue != '';;
-                } else if (rule == 'idCard') {
-                    result = regex['idCard1'].test(inputValue) || regex['idCard2'].test(inputValue);
-                    return result;
-                } else if (rule == 'custom') {
-                    var customRule = RegExp($input.data('rule'));
-                    result = customRule ? customRule.test(inputValue) : true;
-                    return result;
-                } else {
-                    result = regex[rule].test(inputValue);
-                    return result;
+                var selectEle = /select\((.*)\)/.exec(rule)[1], //取出必选几个
+                    $inputName = $input.attr('name'),
+                    sCount = 0,
+                    $input = $('input[name=' + $inputName + ']');
+                $.each($input, function(i, obj) {
+                        if (obj.checked == true) {
+                            sCount++;
+                        }
+                    })
+                    //如果填写的参数大于选项数则默认为选项数
+                if (selectEle >= $input.length) {
+                    selectEle = $input.length;
                 }
+                return sCount < Number(selectEle) ? false : true;
             }
-
-
+        },
+        checkText: function(rule, $input) {
+            var result = true,
+                inputValue = $input.val(); //找到这些input框的值
+            inputValue = $.trim(inputValue); //去掉首尾空格
+            //文本格式验证正则表达式
+            var regex = {
+                email: /^(\w)+(\W\w+)*@(\w)+(\-\w+)*((\.\w+)+)$/, // email
+                phone: /^1[3|4|5|7|8]\d{9}$/, // 手机号码
+                tell: /\d{3}-\d{8}|\d{4}-\d{7,8}/, // 固话
+                number: /^[\-\+]?((\d+)([\.,](\d+))?|([\.,](\d+))+)$/, // 数字
+                integer: /^[\-\+]?((\d+))$/, // 整数
+                url: /^[a-zA-z]+:\/\/(\w+(-\w+)*)(\.(\w+(-\w+)*))*/, // 链接
+                password: /^[\w~!@#$%^&*()_+{}:"<>?\-=[\];\',.\/]{6,30}$/, // 密码
+                postCode: /^\d{6}$/, //邮编
+                idCard1: /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/, //15位身份证
+                idCard2: /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/, //18位身份证
+            };
+            if (rule.search(/confpwd/) > -1) {
+                var ele = /confpwd\((.*)\)/.exec(rule)[1];
+                return inputValue == $(ele).val();
+            } else if (rule == 'empty') {
+                return inputValue != '';;
+            } else if (rule.search(/max/) > -1) {
+                var maxEle = /max\((.*)\)/.exec(rule)[1];
+                return inputValue.length > 0 && inputValue.length <= maxEle ? true : false;
+            } else if (rule.search(/min/) > -1) {
+                var maxEle = /min\((.*)\)/.exec(rule)[1];
+                return inputValue.length > 0 && inputValue.length >= maxEle ? true : false;
+            } else if (rule == 'idCard') {
+                result = regex['idCard1'].test(inputValue) || regex['idCard2'].test(inputValue);
+                return result;
+            } else if (rule == 'custom') {
+                var customRule = RegExp($input.data('rule'));
+                result = customRule ? customRule.test(inputValue) : true;
+                return result;
+            } else {
+                result = regex[rule].test(inputValue);
+                return result;
+            }
+        },
+        check: function(rule, $input) {
+            var self = this,
+                labelType = $input.attr('type'); //找到这些input框的类型
+            //先做类型判断
+            if (labelType == "checkbox") {
+                return self.checkBox(rule, $input);
+            } else {
+                return self.checkText(rule, $input);
+            }
         },
         checkBind: function(rule, $input) {
             var self = this,
-                ruleOption = rule.split('|'), //或判断
+                ruleOption = [], //或判断
                 labelText = $input.data("label") || '', //找到这些input框的label值
-                orResult = true,
                 testRule = true;
+            if (rule.indexOf('|') > 0) {
+                var orCount = 0,
+                    orResult = true;
+                ruleOption = rule.split('|');
 
-            if (ruleOption.length > 1) {
-                var count = 0;
                 $.each(ruleOption, function(i, iObj) {
                     orResult = self.check(iObj, $input);
                     if (orResult) {
-                        count++; //通过验证则累加
+                        orCount++; //通过验证则累加
                     }
                 })
                 rule = 'orCheck';
-                testRule = count > 0 ? true : false;
+                testRule = orCount > 0 ? true : false;
+            } else if (rule.indexOf('&') > 0) {
+                var andCount = 0,
+                    andResult = true;
+                ruleOption = rule.split('&');
+
+                $.each(ruleOption, function(i, iObj) {
+                    andResult = self.check(iObj, $input);
+                    if (andResult) {
+                        andCount++; //通过验证则累加
+                    }
+                })
+                rule = 'andCheck';
+                testRule = andCount == ruleOption.length ? true : false;
             } else {
                 testRule = self.check(rule, $input);
                 if (rule.search(/confpwd/) > -1) {
                     rule = 'confpwd';
+                } else if (rule.search(/select/) > -1) {
+                    rule = 'select';
+                } else if (rule.search(/max/) > -1) {
+                    rule = 'max';
+                } else if (rule.search(/min/) > -1) {
+                    rule = 'min';
                 }
             }
-
             if (!testRule) {
                 self.showTip(rule, labelText);
                 $input.parents('.field').addClass('bg-warning');
-                self.onfocus($input);
+                self.onFocus($input);
                 return false;
             } else {
                 $input.parents('.field').removeClass('bg-warning');
